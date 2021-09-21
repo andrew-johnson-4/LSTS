50ac5990e19a3aede4156413d827b7f8bd15163a
Andrew Johnson <andrew@subarctic.org>
2021-09-20 21:04:28 -0600

    generate judgements from rust input files

--- src/rust/mod.rs
+++ src/rust/mod.rs
@@ -1,7 +1,24 @@
 use std::fs::File;
 use std::io::prelude::*;
+use crate::Judgements;
 
 pub struct RustError;
+
+pub fn judge_item(j: &mut Judgements, syntax: &syn::Item) {
+   match syntax {
+      syn::Item::Fn(f) => {
+         //main: () -> ()
+         let fname = f.sig.ident.to_string();
+         println!("judge fn: {:?}", f.sig);
+      },
+      _ => {}
+   }
+}
+pub fn judge_file(j: &mut Judgements, syntax: &syn::File) {
+   for i in syntax.items.iter() {
+      judge_item(j, i);
+   }
+}
 
 pub fn typecheck_file(path: &str) -> Result<(),RustError>
 {
@@ -9,7 +26,10 @@ pub fn typecheck_file(path: &str) -> Result<(),RustError>
    let mut src = String::new();
    file.read_to_string(&mut src).expect(&format!("Unable to read file: {}", path));
 
-   let _syntax = syn::parse_file(&src).expect(&format!("Unable to parse file: {}", path));
+   let syntax = syn::parse_file(&src).expect(&format!("Unable to parse file: {}", path));
+
+   let mut j = Judgements::zero();
+   judge_file(&mut j, &syntax);
 
    //TODO: load Rust prelude and typecheck file
    Ok(())
