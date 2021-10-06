df993ea0725b4b681485cea3373c47ebd46d5e1a
Andrew Johnson <andrew@subarctic.org>
2021-10-06 15:08:42 -0600

    extract type of literal expressions

--- src/rust/mod.rs
+++ src/rust/mod.rs
@@ -4,6 +4,16 @@ use crate::{Judgements,Type};
 
 pub struct RustError;
 
+pub fn judge_typeof_lit(l: &syn::ExprLit) -> Type {
+   match l.lit {
+      syn::Lit::Str(_) => Type::Ground("str"),
+      syn::Lit::Char(_) => Type::Ground("char"),
+      syn::Lit::Int(_) => Type::Ground("int"),
+      syn::Lit::Float(_) => Type::Ground("float"),
+      syn::Lit::Bool(_) => Type::Ground("bool"),
+      _ => panic!("unrecognized literal type: {:?}", l.lit)
+   }
+}
 pub fn judge_typeof(tt: &syn::Type) -> Type {
    Type::Ground("")
 }
@@ -16,7 +26,10 @@ pub fn judge_stmt(j: &mut Judgements, syntax: &syn::Stmt) {
                let imut = i.mutability.is_some();
                let ident = i.ident.to_string();
                match &l.init {
-                  Some((_,e)) => {
+//ident local "x" = Lit(ExprLit { attrs: [], lit: Int(LitInt { token: 5 }) });
+                  Some((_,box syn::Expr::Lit(l))) => {
+                     println!("ident local literal {}{}{:?} = {};", if iref {"ref "} else {""}, if imut {"mut "} else {""}, ident, judge_typeof_lit(l))
+                  }, Some((_,e)) => {
                      println!("ident local {}{}{:?} = {:?};", if iref {"ref "} else {""}, if imut {"mut "} else {""}, ident, e)
                   },
                   None => println!("ident local {}{}{:?};", if iref {"ref "} else {""}, if imut {"mut "} else {""}, ident)
