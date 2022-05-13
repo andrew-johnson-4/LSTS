d139c855b7e7857611fc6b93d91b799be253fd82
Andrew Johnson <andrew@subarctic.org>
2022-05-13 16:46:28 -0600

    lay out some more tests

--- tests/tlc.rs
+++ tests/tlc.rs
@@ -3,7 +3,16 @@ use lsts::syntax::TLC;
 fn rust_simplytyped() {
    assert!(TLC::parse("let t: T").is_ok());
    assert!(TLC::parse("let t: 1").is_ok());
+   assert!(TLC::parse("a()").is_ok());
+   assert!(TLC::parse("a(b)").is_ok());
+   assert!(TLC::parse("a(b,c)").is_ok());
+   assert!(TLC::parse("let t: ()").is_ok());
+   assert!(TLC::parse("let t: (A)").is_ok());
+   assert!(TLC::parse("let t: (A,B)").is_ok());
    assert!(TLC::parse("let t: T<1,2>").is_ok());
    assert!(TLC::parse("let t: [1]").is_ok());
-   assert!(TLC::parse("let t: 1->2").is_ok());
+   assert!(TLC::parse("let t: ()->A").is_ok());
+   assert!(TLC::parse("let t: A->B").is_ok());
+   assert!(TLC::parse("let t: (A)->B").is_ok());
+   assert!(TLC::parse("let t: (A,B)->B").is_ok());
 }
