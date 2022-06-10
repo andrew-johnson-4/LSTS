2459b33cbf8ee05e8ccac413a117ead330e381a5
Andrew Johnson <andrew@subarctic.org>
2022-06-10 16:20:09 -0600

    create tests for longer term goal of advanced type-theoretic problems

---
+++ tests/formula.rs
@@ -0,0 +1,5 @@
+
+/* TODO: introduce type priority over ambiguity.
+   1. Create some ambiguous typing rules.
+   2. Define some selection criteria for typing priority.
+   3. Accept/Reject ambiguous constructions that can/can't be ordered. */
