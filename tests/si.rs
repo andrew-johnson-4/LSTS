cad8eda6531d8048da9135dbf6d4a9120e60bcb1
Andrew Johnson <andrew@subarctic.org>
2022-06-11 16:04:00 -0600

    add test for inferring types of constant literals

--- tests/si.rs
+++ tests/si.rs
@@ -1,6 +1,39 @@
 use lsts::tlc::TLC;
 
 #[test]
+fn check_constant_literals() {
+   //soft cast literals into typed values
+   let mut tlc = TLC::new();
+   let si = tlc.compile_file(None, "preludes/si.tlc").unwrap();
+   tlc.check(Some(si), "True:Boolean").unwrap();
+   tlc.check(Some(si), "False:Boolean").unwrap();
+   tlc.check(Some(si), "1:Boolean").unwrap_err();
+
+   tlc.check(Some(si), "0:Integer").unwrap();
+   tlc.check(Some(si), "321:Integer").unwrap();
+   tlc.check(Some(si), "3e2:Integer").unwrap();
+   tlc.check(Some(si), "3.2:Integer").unwrap_err();
+   tlc.check(Some(si), "3+1i:Integer").unwrap_err();
+   tlc.check(Some(si), "True:Integer").unwrap_err();
+
+   tlc.check(Some(si), "0:Real").unwrap();
+   tlc.check(Some(si), "321:Real").unwrap();
+   tlc.check(Some(si), "3e2:Real").unwrap();
+   tlc.check(Some(si), "3.2:Real").unwrap();
+   tlc.check(Some(si), "3+1i:Real").unwrap_err();
+   tlc.check(Some(si), "3.2+1i:Real").unwrap_err();
+   tlc.check(Some(si), "True:Real").unwrap_err();
+
+   tlc.check(Some(si), "0:Complex").unwrap();
+   tlc.check(Some(si), "321:Complex").unwrap();
+   tlc.check(Some(si), "3e2:Complex").unwrap();
+   tlc.check(Some(si), "3.2:Complex").unwrap();
+   tlc.check(Some(si), "3+1i:Complex").unwrap();
+   tlc.check(Some(si), "3.2+1i:Complex").unwrap();
+   tlc.check(Some(si), "True:Complex").unwrap_err();
+}
+
+#[test]
 fn check_type_equality() {
    //Ground Units unify with themselves
    let mut tlc = TLC::new();
