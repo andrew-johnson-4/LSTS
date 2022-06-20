1a52ecff0c76ab52dbef864e61eca2e1e5984f44
Andrew Johnson <andrew@subarctic.org>
2022-06-19 20:18:02 -0600

    this proof isnt so hard

---
+++ tests/infinitude_of_primes.rs
@@ -0,0 +1,19 @@
+use lsts::tlc::TLC;
+
+#[test]
+fn check_infinitude_of_primes() {
+/*
+   let mut tlc = TLC::new();
+   let sa = tlc.import_str(None, r#"
+   //define a prime as being indivisible by any other integer
+   type Prime:Integer where d:Prime, n:Integer. d%n != 0;
+"#).unwrap();
+
+   //prove that there are an infinite number of primes
+   tlc.check(Some(sa), r#"
+   let primes:Prime[];           //assume there are a finite number of primes
+   let p = primes.product() + 1; //let p be the product of all primes + 1
+   forall d:primes. p%d == 1;    //p mod d, forall d in primes list, is 1
+   "#).unwrap();
+*/
+}
