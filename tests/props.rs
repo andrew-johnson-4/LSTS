8496fc3a1387f0e73246b3db95ab303a25d86911
Andrew Johnson <andrew@subarctic.org>
2022-06-20 17:20:38 -0600

    make test asserting that a proposition cant be both True and False

--- tests/props.rs
+++ tests/props.rs
@@ -33,5 +33,15 @@ fn check_boolean() {
    tlc.check(Some(si), "True || False:False").unwrap_err();
    tlc.check(Some(si), "False || True:False").unwrap_err();
    tlc.check(Some(si), "False || False:False").unwrap();
+}
 
+#[test]
+fn check_contradictions() {
+   let mut tlc = TLC::new();
+   let si = tlc.import_file(None, "preludes/si.tlc").unwrap();
+
+   //Boolean Soundness
+   tlc.check(Some(si), "let a:True").unwrap();
+   tlc.check(Some(si), "let a:False").unwrap();
+   tlc.check(Some(si), "let a:True+False").unwrap_err();
 }
