913467920eeeafdc8002cb9fe7dae0aae051d8b6
Andrew Johnson <andrew@subarctic.org>
2022-06-19 18:51:16 -0600

    assert that proof is OK up until reductio ad absurdum

--- tests/sqrt_two.rs
+++ tests/sqrt_two.rs
@@ -17,4 +17,13 @@ square(sqrt_of_two) * square(q): Pt*Pt; //2 * q*q = p*p
 square(p) / square(sqrt_of_two): Qt*Qt; //p*p / 2 = q*q
 p / square(sqrt_of_two) : ?/();         //2 is a factor of p
 "#).unwrap_err();
+
+   //assert that proof is OK until absurd final statement
+   tlc.check(Some(sa), r#"
+type Pt; let p:Pt;
+type Qt; let q:Qt;
+let sqrt_of_two: Pt/Qt;
+square(sqrt_of_two) * square(q): Pt*Pt; //2 * q*q = p*p
+square(p) / square(sqrt_of_two): Qt*Qt; //p*p / 2 = q*q
+"#).unwrap();
 }
