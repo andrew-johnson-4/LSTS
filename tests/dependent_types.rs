386c4444ae4675b2ea979f7305609a984c39084c
Andrew Johnson <andrew@subarctic.org>
2022-06-21 16:31:23 -0600

    add some constant reductions that should be rejected to ensure dependent types arent just being ignored

--- tests/dependent_types.rs
+++ tests/dependent_types.rs
@@ -8,13 +8,18 @@ fn check_constant_numbers() {
    tlc.check(Some(si), "0: [0]").unwrap();
    tlc.check(Some(si), "1: [1]").unwrap();
    tlc.check(Some(si), "2: [2]").unwrap();
+   tlc.check(Some(si), "0: [1]").unwrap_err();
+   tlc.check(Some(si), "0: [2]").unwrap_err();
+   tlc.check(Some(si), "1: [0]").unwrap_err();
 
    tlc.check(Some(si), "-0: [0]").unwrap();
    tlc.check(Some(si), "-1: [-1]").unwrap();
    tlc.check(Some(si), "-2: [-2]").unwrap();
+   tlc.check(Some(si), "-2: [2]").unwrap_err();
    tlc.check(Some(si), "-(-0): [0]").unwrap();
    tlc.check(Some(si), "-(-1): [1]").unwrap();
    tlc.check(Some(si), "-(-2): [2]").unwrap();
+   tlc.check(Some(si), "-(-1): [-1]").unwrap_err();
 
    tlc.check(Some(si), "0+0: [0]").unwrap();
    tlc.check(Some(si), "0+1: [1]").unwrap();
@@ -23,6 +28,8 @@ fn check_constant_numbers() {
    tlc.check(Some(si), "2+1: [3]").unwrap();
    tlc.check(Some(si), "1+2: [3]").unwrap();
    tlc.check(Some(si), "2+2: [4]").unwrap();
+   tlc.check(Some(si), "1+1: [3]").unwrap_err();
+   tlc.check(Some(si), "2+2: [5]").unwrap_err();
 
    tlc.check(Some(si), "0-0: [0]").unwrap();
    tlc.check(Some(si), "0-1: [-1]").unwrap();
@@ -31,6 +38,8 @@ fn check_constant_numbers() {
    tlc.check(Some(si), "2-1: [1]").unwrap();
    tlc.check(Some(si), "1-2: [-1]").unwrap();
    tlc.check(Some(si), "2-2: [0]").unwrap();
+   tlc.check(Some(si), "5-2: [0]").unwrap_err();
+   tlc.check(Some(si), "2-5: [1]").unwrap_err();
 
    tlc.check(Some(si), "0*0: [0]").unwrap();
    tlc.check(Some(si), "0*1: [0]").unwrap();
@@ -39,6 +48,8 @@ fn check_constant_numbers() {
    tlc.check(Some(si), "2*1: [2]").unwrap();
    tlc.check(Some(si), "1*2: [2]").unwrap();
    tlc.check(Some(si), "2*2: [4]").unwrap();
+   tlc.check(Some(si), "2*2: [5]").unwrap_err();
+   tlc.check(Some(si), "1*3: [4]").unwrap_err();
 
    tlc.check(Some(si), "0/0: [NaN]").unwrap();
    tlc.check(Some(si), "0/1: [0]").unwrap();
@@ -50,6 +61,8 @@ fn check_constant_numbers() {
    tlc.check(Some(si), "3/2: [1]").unwrap();
    tlc.check(Some(si), "4/2: [2]").unwrap();
    tlc.check(Some(si), "5/2: [2]").unwrap();
+   tlc.check(Some(si), "5/0: [2]").unwrap_err();
+   tlc.check(Some(si), "5/2: [3]").unwrap_err();
 
    tlc.check(Some(si), "0%0: [NaN]").unwrap();
    tlc.check(Some(si), "0%1: [0]").unwrap();
@@ -61,4 +74,7 @@ fn check_constant_numbers() {
    tlc.check(Some(si), "3%2: [1]").unwrap();
    tlc.check(Some(si), "4%2: [0]").unwrap();
    tlc.check(Some(si), "5%2: [1]").unwrap();
+   tlc.check(Some(si), "5%2: [7]").unwrap_err();
+   tlc.check(Some(si), "0%1: [NaN]").unwrap_err();
+   tlc.check(Some(si), "0%0: [0]").unwrap_err();
 }
