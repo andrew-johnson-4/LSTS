2f148461d03e7bd1547cf99c8edb93ffd456fc52
Andrew Johnson <andrew@subarctic.org>
2022-06-23 21:40:18 -0600

    unification clobbering example to open ticket

---
+++ tests/open_issues.rs
@@ -0,0 +1,12 @@
+use lsts::tlc::TLC;
+
+#[test]
+fn check_unification_clobbering() {
+   let mut tlc = TLC::new();
+   let si = tlc.import_file(None, "preludes/si.tlc").unwrap();
+
+   tlc.check(Some(si), "-(-(1)): [1]").unwrap();
+   tlc.check(Some(si), "-(-1): [1]").unwrap();
+   tlc.check(Some(si), "-(-2): [2]").unwrap();
+   tlc.check(Some(si), "-(-1): [-1]").unwrap_err();
+}
