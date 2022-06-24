805373715c91651ca6a990a2d7d06d30c766aa62
Andrew Johnson <andrew@subarctic.org>
2022-06-24 16:49:14 -0600

    move tensor invariants test out of tensor syntax test

--- tests/tensor.rs
+++ tests/tensor.rs
@@ -11,6 +11,12 @@ fn check_tensor_syntax() {
    tlc.check(Some(si), "let a:Number[1][]").unwrap();
    tlc.check(Some(si), "let a:Number[][2]").unwrap();
    tlc.check(Some(si), "let a:Number[][2]").unwrap();
+}
+
+#[test]
+fn check_tensor_invariants() {
+   let mut tlc = TLC::new();
+   let si = tlc.import_file(None, "preludes/si.tlc").unwrap();
 
    tlc.check(Some(si), "let a:Number[1]; a.length==0: [True]").unwrap_err();
    tlc.check(Some(si), "let a:Number[1]; a.length==0: [False]").unwrap();
