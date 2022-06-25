669ef3f309062f85ef7a11414d038a2c6502ca90
Andrew Johnson <andrew@subarctic.org>
2022-06-25 05:43:33 -0600

    inhabited types must prove invariants

--- tests/prime.rs
+++ tests/prime.rs
@@ -6,4 +6,11 @@ fn check_properties_of_primes() {
    let si = tlc.import_file(None, "preludes/si.tlc").unwrap();
 
    tlc.check(Some(si), "let p:Prime").unwrap();
+   tlc.check(Some(si), "let p:Prime = 0").unwrap_err();
+   tlc.check(Some(si), "let p:Prime = 1").unwrap_err();
+   tlc.check(Some(si), "let p:Prime = 2").unwrap();
+   tlc.check(Some(si), "let p:Prime = 3").unwrap();
+   tlc.check(Some(si), "let p:Prime = 4").unwrap_err();
+   tlc.check(Some(si), "let p:Prime = 5").unwrap();
+   tlc.check(Some(si), "let p:Prime = -1").unwrap_err();
 }
