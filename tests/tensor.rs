4247181c0bfac7e776357b0952ac9c3762e0928a
Andrew Johnson <andrew@subarctic.org>
2022-06-25 19:48:19 -0600

    add test for Tensor sugared syntax

--- tests/tensor.rs
+++ tests/tensor.rs
@@ -10,7 +10,18 @@ fn check_tensor_syntax() {
    tlc.check(Some(si), "let a:Number[1][2]").unwrap();
    tlc.check(Some(si), "let a:Number[1][]").unwrap();
    tlc.check(Some(si), "let a:Number[][2]").unwrap();
-   tlc.check(Some(si), "let a:Number[][2]").unwrap();
+}
+
+#[test]
+fn check_tensor_sugar() {
+   let mut tlc = TLC::new();
+   let si = tlc.import_file(None, "preludes/si.tlc").unwrap();
+
+   tlc.check(Some(si), "let a:Number[]; a: Tensor<Number,?>").unwrap();
+   tlc.check(Some(si), "let a:Number[1]; a: Tensor<Number,[1]>").unwrap();
+   tlc.check(Some(si), "let a:Number[1][2]; a: Tensor<Tensor<Number,[2]>,[1]>").unwrap();
+   tlc.check(Some(si), "let a:Number[1][]; a: Tensor<Tensor<Number,[1]>,?>").unwrap();
+   tlc.check(Some(si), "let a:Number[][2]; a: Tensor<Tensor<Number,?>,[2]>").unwrap();
 }
 
 #[test]
