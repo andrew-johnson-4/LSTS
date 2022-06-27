cc4ea0c42ece41c04cddb679f88b0c3455ef7d76
Andrew Johnson <andrew@subarctic.org>
2022-06-27 10:50:54 -0600

    add test for prime factors

--- tests/props.rs
+++ tests/props.rs
@@ -12,3 +12,15 @@ fn check_contradictions() {
    tlc.check(Some(si), "let a:False").unwrap();
    tlc.check(Some(si), "let a:True+False").unwrap_err();
 }
+
+#[test]
+fn check_prime_factors() {
+   let mut tlc = TLC::new();
+   let si = tlc.import_file(None, "preludes/si.tlc").unwrap();
+
+   tlc.check(Some(si), "let x:Prime = 0").unwrap_err();
+   tlc.check(Some(si), "let x:Prime = 1").unwrap_err();
+   tlc.check(Some(si), "let x:Prime = 2").unwrap();
+   tlc.check(Some(si), "let x:Prime = 3").unwrap();
+   tlc.check(Some(si), "let x:Prime = 4").unwrap_err();
+}
