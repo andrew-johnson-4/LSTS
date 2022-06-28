4452d246b7dab72b203767a5aeed08c7311429e6
Andrew Johnson <andrew@subarctic.org>
2022-06-27 21:26:13 -0600

    add syntax for alpha-conversion in dependent contexts

---
+++ tests/algebra.rs
@@ -0,0 +1,13 @@
+use lsts::tlc::TLC;
+
+#[test]
+fn check_alpha_conversion() {
+   let mut tlc = TLC::new();
+
+   tlc.check(None, "let a:[self+2]; a\\[self|3]:[5]").unwrap();
+   tlc.check(None, "let a:[self+2]; a\\[self|3]:[6]").unwrap_err();
+   tlc.check(None, "let a:[self%2==0]; a\\[self%2|0]:[True]").unwrap();
+   tlc.check(None, "let a:[self%2==0]; a\\[self%2|0]:[False]").unwrap_err();
+   tlc.check(None, "let a:[self%2==0]; a\\[self%2|1]:[True]").unwrap_err();
+   tlc.check(None, "let a:[self%2==0]; a\\[self%2|1]:[False]").unwrap();
+}
