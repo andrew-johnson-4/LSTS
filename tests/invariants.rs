71a63b7ca1492e0e553655178428074db10de3d5
Andrew Johnson <andrew@subarctic.org>
2022-07-01 15:51:12 -0600

    now to prove some stuff

--- tests/invariants.rs
+++ tests/invariants.rs
@@ -26,7 +26,6 @@ fn check_postcondition() {
    tlc.check(Some(si), "let a:Even; a%2:[1]").unwrap_err();
 }
 
-/*
 #[test]
 fn check_proof_lines_precondition() {
    let mut tlc = TLC::new();
@@ -40,7 +39,6 @@ fn check_proof_lines_precondition() {
    tlc.check(Some(si), "let a:Prime = 5;").unwrap();
    tlc.check(Some(si), "let a:Prime = 6;").unwrap_err();
 }
-*/
 
 /*
 #[test]
