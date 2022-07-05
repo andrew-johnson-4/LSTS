7a406b8c848ac3a0bb71b14d128f81b300b8651a
Andrew Johnson <andrew@subarctic.org>
2022-07-05 13:11:54 -0600

    start documenting the project

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
+//! Large Scale Type Systems (LSTS)
+//! LSTS implements a categorical view of typed lambda calculus with flexible soundness guarantees
+
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
-//! Large Scale Type Systems (LSTS)
-//! LSTS implements a categorical view of typed lambda calculus with flexible soundness guarantees
 
 /// The TLC module provides a context object for parsing, typing, and evaluating expressions
 pub mod tlc;
