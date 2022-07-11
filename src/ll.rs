87799be991d449fbb31b115737da5a1d44bbd4d1
Andrew Johnson <andrew@subarctic.org>
2022-07-11 11:13:39 -0600

    implement ll1_stmt

--- src/ll.rs
+++ src/ll.rs
@@ -28,20 +28,29 @@ fn pop_is(rule: &str, tokens: &mut Vec<Token>, is: &Vec<Symbol>) -> Result<(),Er
    Ok(())
 }
 
-pub fn ll1_stmt(_tlc: &mut TLC, _scope: ScopeId, _tokens: &mut Vec<Token>) -> Result<TermId,Error> {
-   todo!("implement ll1_stmt")
+pub fn ll1_typ_stmt(_tlc: &mut TLC, _scope: ScopeId, _tokens: &mut Vec<Token>) -> Result<TermId,Error> {
+   todo!("implement ll1_typ_stmt")
+}
+pub fn ll1_forall_stmt(_tlc: &mut TLC, _scope: ScopeId, _tokens: &mut Vec<Token>) -> Result<TermId,Error> {
+   todo!("implement ll1_forall_stmt")
+}
+pub fn ll1_term(_tlc: &mut TLC, _scope: ScopeId, _tokens: &mut Vec<Token>) -> Result<TermId,Error> {
+   todo!("implement ll1_term")
 }
 
-pub fn ll1_file(tlc: &mut TLC, scope: ScopeId, tokens: &mut Vec<Token>) -> Result<TermId,Error> {
-   let mut es = Vec::new();
-   while !peek_is(tokens, &vec![Symbol::EOF]) {
-      es.push( ll1_stmt(tlc, scope, tokens)? );
+pub fn ll1_stmt(tlc: &mut TLC, scope: ScopeId, tokens: &mut Vec<Token>) -> Result<TermId,Error> {
+   if peek_is(tokens, &vec![Symbol::LeftBrace]) {
+      ll1_block_stmt(tlc, scope, tokens)
+   } else if peek_is(tokens, &vec![Symbol::Type]) {
+      ll1_typ_stmt(tlc, scope, tokens)
+   } else if peek_is(tokens, &vec![Symbol::Forall]) {
+      ll1_forall_stmt(tlc, scope, tokens)
+   } else {
+      ll1_term(tlc, scope, tokens)
    }
-   pop_is("file", tokens, &vec![Symbol::EOF])?;
-   Ok(tlc.push_term(Term::Block(scope,es), &span_of(tokens)))
 }
 
-pub fn ll1_block(tlc: &mut TLC, scope: ScopeId, tokens: &mut Vec<Token>) -> Result<TermId,Error> {
+pub fn ll1_block_stmt(tlc: &mut TLC, scope: ScopeId, tokens: &mut Vec<Token>) -> Result<TermId,Error> {
    let scope = tlc.push_scope(Scope {
       parent: Some(scope),
       children: Vec::new(),
@@ -56,3 +65,13 @@ pub fn ll1_block(tlc: &mut TLC, scope: ScopeId, tokens: &mut Vec<Token>) -> Resu
 
    Ok(tlc.push_term(Term::Block(scope,es), &span_of(tokens)))
 }
+
+pub fn ll1_file(tlc: &mut TLC, scope: ScopeId, tokens: &mut Vec<Token>) -> Result<TermId,Error> {
+   let mut es = Vec::new();
+   while !peek_is(tokens, &vec![Symbol::EOF]) {
+      es.push( ll1_stmt(tlc, scope, tokens)? );
+   }
+   pop_is("file", tokens, &vec![Symbol::EOF])?;
+   Ok(tlc.push_term(Term::Block(scope,es), &span_of(tokens)))
+}
+
