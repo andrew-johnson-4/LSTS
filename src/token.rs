104d4223e99cfffffe8b501597ab92d30fec57b6
Andrew Johnson <andrew@subarctic.org>
2022-07-10 18:26:46 -0600

    tokenize some operators

--- src/token.rs
+++ src/token.rs
@@ -30,11 +30,13 @@ pub fn span_of(ts: &Vec<Token>) -> Span {
    }
 }
 
+#[derive(Clone)]
 pub struct Token {
    pub symbol: Symbol,
    pub span: Span,
 }
 
+#[derive(Clone)]
 pub enum Symbol {
    Ident(String),
    Equal,
@@ -43,6 +45,8 @@ pub enum Symbol {
    GreaterThanOrEqual,
    LessThan,
    LessThanOrEqual,
+   Ascript,
+   KAscript,
    Typeof,
    As,
    If,
@@ -57,6 +61,8 @@ impl std::fmt::Debug for Symbol {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
            Symbol::Ident(s)           => write!(f, r#"$"{}""#, s),
+           Symbol::Ascript            => write!(f, ":"),
+           Symbol::KAscript           => write!(f, "::"),
            Symbol::Equal              => write!(f, "="),
            Symbol::NotEqual           => write!(f, "!="),
            Symbol::GreaterThan        => write!(f, ">"),
@@ -84,63 +90,82 @@ pub fn is_ident_char(source: &str, index: usize) -> bool {
 pub fn tokenize(source_name:String, source: &str) -> Result<Vec<Token>,Error> {
    let filename = Rc::new(source_name);
    let mut tokens = Vec::new();
-   let mut source_index = 0;
+   let mut si = 0;
    let mut line = 1;
    let mut column = 1;
-   while source_index < source.len() {
-      let mut advanced = false;
-      match source.as_bytes()[source_index] as char {
-        ' ' => { column += 1; },
-        '\n' => { column = 1; line += 1 },
-        'a'..='z' => {
-           let start = source_index;
-           let mut ci = source_index + 1;
-           while is_ident_char(source, ci) {
-              ci += 1;
-           }
-           let start_column = column;
-           column += ci - start;
-           let span = Span {
-              filename: filename.clone(),
-              offset_start: start,
-              offset_end: ci,
-              linecol_start: (line,start_column),
-              linecol_end: (line,column), 
-           };
-           let ident = std::str::from_utf8(&source.as_bytes()[start..ci]).unwrap();
-           advanced = true;
-           source_index = ci;
-	   match ident {
-              "typeof" => { tokens.push(Token { symbol: Symbol::Typeof, span: span, }); },
-              "as" => { tokens.push(Token { symbol: Symbol::As, span: span, }); },
-              "if" => { tokens.push(Token { symbol: Symbol::If, span: span, }); },
-              "else" => { tokens.push(Token { symbol: Symbol::Else, span: span, }); },
-              "let" => { tokens.push(Token { symbol: Symbol::Let, span: span, }); },
-              "forall" => { tokens.push(Token { symbol: Symbol::Forall, span: span, }); },
-              "type" => { tokens.push(Token { symbol: Symbol::Type, span: span, }); },
-              "normal" => { tokens.push(Token { symbol: Symbol::Normal, span: span, }); },
-              "where" => { tokens.push(Token { symbol: Symbol::Where, span: span, }); },
-              _ => { tokens.push(Token { symbol: Symbol::Ident(ident.to_string()), span: span, }); },
-           }
-           eprintln!("push symbol {:?}", tokens[tokens.len()-1].symbol);
-        },
+   let operators: Vec<(&str,Symbol)> = vec![
+      ("::",Symbol::KAscript),
+      (":", Symbol::Ascript),
+      ("==",Symbol::Equal),
+      ("!=",Symbol::NotEqual),
+      (">=",Symbol::GreaterThanOrEqual),
+      (">", Symbol::GreaterThan),
+      ("<=",Symbol::LessThanOrEqual),
+      ("<", Symbol::LessThan),
+   ];
+   fn span_of(filename:&Rc<String>, start_index:usize, token_length:usize, line:usize, column:usize) -> Span {
+      Span {
+         filename: filename.clone(),
+         offset_start: start_index,
+         offset_end: start_index + token_length,
+         linecol_start: (line,column),
+         linecol_end: (line,column + token_length),
+      }
+   }
+   while si < source.len() {
+      match source.as_bytes()[si] as char {
+         ' ' => { column += 1; si += 1; },
+         '\n' => { column = 1; line += 1; si += 1; },
+         'a'..='z' => {
+            let mut ci = si + 1;
+            while ci<source.len() && is_ident_char(source, ci) {
+               ci += 1;
+            }
+            let span = span_of(&filename, si, ci - si, line, column);
+            column += ci - si;
+            let ident = std::str::from_utf8(&source.as_bytes()[si..ci]).unwrap();
+            match ident {
+               "typeof" => { tokens.push(Token { symbol: Symbol::Typeof, span: span, }); },
+               "as" => { tokens.push(Token { symbol: Symbol::As, span: span, }); },
+               "if" => { tokens.push(Token { symbol: Symbol::If, span: span, }); },
+               "else" => { tokens.push(Token { symbol: Symbol::Else, span: span, }); },
+               "let" => { tokens.push(Token { symbol: Symbol::Let, span: span, }); },
+               "forall" => { tokens.push(Token { symbol: Symbol::Forall, span: span, }); },
+               "type" => { tokens.push(Token { symbol: Symbol::Type, span: span, }); },
+               "normal" => { tokens.push(Token { symbol: Symbol::Normal, span: span, }); },
+               "where" => { tokens.push(Token { symbol: Symbol::Where, span: span, }); },
+               _ => { tokens.push(Token { symbol: Symbol::Ident(ident.to_string()), span: span, }); },
+            }
+            si = ci;
+         },
          _ => {
-            return Err(Error{
+            let mut found_operator = false;
+            for (tok,sym) in operators.iter() {
+               if &source.as_bytes()[si..si+tok.len()] == tok.as_bytes() {
+                  let span = span_of(&filename, si, tok.len(), line, column);
+                  tokens.push(Token { symbol: sym.clone(), span: span, });
+                  column += tok.len();
+                  si += tok.len();
+                  found_operator = true;
+                  break;
+               }
+            }
+            if !found_operator { return Err(Error{
                kind: "Tokenization Error".to_string(),
-               rule: format!("Unexpected Character '{}'", if source_index<source.len() 
-                            { (source.as_bytes()[source_index] as char).to_string() }
+               rule: format!("Unexpected Character '{}'", if si<source.len() 
+                            { (source.as_bytes()[si] as char).to_string() }
                        else {"EOF".to_string()} ),
                span: Span {
                   filename: filename.clone(),
-                  offset_start: source_index,
-                  offset_end: source_index+1,
+                  offset_start: si,
+                  offset_end: si+1,
                   linecol_start: (line,column),
                   linecol_end: (line,column+1),
                },
-            })
+            }); }
          },
       };
-      if !advanced { source_index += 1; }
+      eprintln!("push symbol {:?}", tokens[tokens.len()-1].symbol);
    }
    Ok(tokens)
 }
