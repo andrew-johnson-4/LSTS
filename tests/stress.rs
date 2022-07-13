def9b0c8b91b00f234ee6ef4c24f1a56ad1eb8d9
Andrew Johnson <andrew@subarctic.org>
2022-07-13 09:19:56 -0600

    you get 10 lines. 10.

--- tests/stress.rs
+++ tests/stress.rs
@@ -11,10 +11,32 @@ fn parse_1() {
 }
 
 #[test]
+fn parse_10() {
+   let mut tlc = TLC::new();
+   let si = tlc.import_file(None, "preludes/si.tlc").unwrap();
+   if Path::new("tests/stress/10.tlc").exists() {
+      tlc.import_file(Some(si), "tests/stress/10.tlc").unwrap();
+   }
+}
+
+/*
+#[test]
+fn parse_100() {
+   let mut tlc = TLC::new();
+   let si = tlc.import_file(None, "preludes/si.tlc").unwrap();
+   if Path::new("tests/stress/100.tlc").exists() {
+      tlc.import_file(Some(si), "tests/stress/100.tlc").unwrap();
+   }
+}
+*/
+
+/*
+#[test]
 fn parse_1k() {
    let mut tlc = TLC::new();
    let si = tlc.import_file(None, "preludes/si.tlc").unwrap();
-   if Path::new("tests/stress/1.tlc").exists() {
+   if Path::new("tests/stress/1k.tlc").exists() {
       tlc.import_file(Some(si), "tests/stress/1k.tlc").unwrap();
    }
 }
+*/
