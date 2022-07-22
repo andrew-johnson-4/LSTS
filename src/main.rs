807df6f5b9f9c2c288a2b22f34764160cf108d05
Andrew Johnson <andrew@subarctic.org>
2022-07-21 19:06:38 -0600

    improve command line experience

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,24 @@ use lsts::tlc::TLC;
 
 fn main() {
    let mut tlc = TLC::new();
-   let mut env = None;
+   let mut command = "help".to_string();
+   let mut args = Vec::new();
    for (argi,argument) in env::args().enumerate() {
       if argi==0 { continue; }
-      env = Some(tlc.import_file(env, &argument).unwrap());
+      if argi==1 { command = argument.clone(); continue; }
+      args.push(argument.clone());
+   }
+   if command=="build" {
+      let mut env = None;
+      for fp in args.iter() {
+         println!("Compiling: {}", fp);
+         env = Some(tlc.import_file(env, fp).unwrap());
+      }
+   } else {
+      println!("lsts help");
+      println!("     parse [filenames] -- parse files but nothing more");
+      println!("     check [filenames] -- parse and typecheck files");
+      println!("     build [filenames] -- compile provided files as a program");
+      println!("");
    }
 }
