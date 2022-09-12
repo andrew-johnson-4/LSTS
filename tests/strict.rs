99caa0df70f39ba929890a78915be295a0ad7df9
Andrew Johnson <andrew@subarctic.org>
2022-09-12 15:30:38 -0600

    prevent normal terms

---
+++ tests/strict.rs
@@ -0,0 +1,8 @@
+use lsts::tlc::TLC;
+
+#[test]
+fn parse_simplytyped() {
+   let mut tlc = TLC::new();
+   assert!( tlc.parse("type A").is_ok() );
+   assert!( tlc.parse("type normal A").is_err() );
+}
