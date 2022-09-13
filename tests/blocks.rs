1f699f461ee092a3f1f9e5319e7c3c61d09e83a3
Andrew Johnson <andrew@subarctic.org>
2022-09-13 15:07:43 -0600

    curly blocks already work but have a syntax conflict with sets and maps that needs to be resolved

---
+++ tests/blocks.rs
@@ -0,0 +1,14 @@
+use lsts::tlc::TLC;
+
+#[test]
+fn check_properties_of_primes() {
+   let mut tlc = TLC::new();
+   let si = tlc.import_file(None, "preludes/si.tlc").unwrap();
+
+   tlc.check(Some(si), "{}").unwrap();
+   tlc.check(Some(si), "{();}").unwrap();
+   tlc.check(Some(si), "{();()}").unwrap();
+   tlc.check(Some(si), "{};").unwrap();
+   tlc.check(Some(si), "{();};").unwrap();
+   tlc.check(Some(si), "{();()};").unwrap();
+}
