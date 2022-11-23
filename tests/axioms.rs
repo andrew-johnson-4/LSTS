533eb7af65f20083228f5c9f06c95bfcdf5c43db
Andrew Johnson <andrew@subarctic.org>
2022-11-22 19:39:18 -0700

    add test case for axiom vs forall treatment

--- tests/axioms.rs
+++ tests/axioms.rs
@@ -8,3 +8,12 @@ fn algebra1() {
    tlc.check(Some(alg), "1 + 2 * 3 == 1 + 2 * 3 @reflexive : [True]").unwrap();
    tlc.check(Some(alg), "2 * 3 + 1 == 1 + 2 * 3 @reflexive : [True]").unwrap_err();
 }
+
+#[test]
+fn unfounded1() {
+   let mut tlc = TLC::new().strict();
+   let alg = tlc.import_file(None, "preludes/algebra.tlc").unwrap();
+
+   tlc.check(Some(alg), "axiom  @true. [True] = 1; 1 @true : [True]").unwrap();
+   tlc.check(Some(alg), "forall @true. [True] = 1; 1 @true : [True]").unwrap_err();
+}
