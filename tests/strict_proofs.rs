95bb60b985f081190a2b7f5a6d9ed6b41fda18d1
Andrew Johnson <andrew@subarctic.org>
2022-11-28 17:43:12 -0700

    introduce implicit hint @reduce

--- tests/strict_proofs.rs
+++ tests/strict_proofs.rs
@@ -5,6 +5,6 @@ fn one_plus_one_equals_two() {
    let mut tlc = TLC::new().strict();
    let alg = tlc.import_file(None, "preludes/algebra.tlc").unwrap();
 
-   tlc.check(Some(alg), "1 + 1 == 2 : [True]").unwrap();
-   tlc.check(Some(alg), "1 + 2 == 3 : [True]").unwrap_err();
+   tlc.check(Some(alg), "1 + 1 == 2 @reduce : [True]").unwrap();
+   tlc.check(Some(alg), "1 + 2 == 3 @reduce : [True]").unwrap_err();
 }
