61f47970d42fb8d0ea80e46a0386f86731aacdb9
Andrew Johnson <andrew@subarctic.org>
2022-11-29 01:27:23 -0700

    define DFA struct

--- src/dfa.rs
+++ src/dfa.rs
@@ -1,7 +1,15 @@
 
+#[derive(Clone)]
 pub enum LiteralPattern {
    Char(char),
    String(String),
    Range(String),
    Variable(String),
+}
+
+#[derive(Clone)]
+pub struct DFA {
+   start: usize,
+   states: Vec<bool>, //bool = is accept
+   transitions: Vec<(usize,char,usize)>,
 }
