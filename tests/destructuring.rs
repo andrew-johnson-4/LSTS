5f486df5bb8f4ad9974b6ca2e8385210d00d695a
Andrew Johnson <andrew@subarctic.org>
2022-12-02 19:50:44 -0700

    add another failing test case for destructuring pattern matching

---
+++ tests/destructuring.rs
@@ -0,0 +1,22 @@
+use lsts::tlc::TLC;
+
+#[test]
+fn check_match_literal() {
+   let mut tlc = TLC::new();
+   let l1 = tlc.import_file(None, "preludes/l1.tlc").unwrap();
+
+   tlc.check(Some(l1), "match 1 { 1 => 1, 2 => 2, _ => 3 } @reduce : [1];").unwrap();
+   tlc.check(Some(l1), "match 2 { 1 => 1, 2 => 2, _ => 3 } @reduce : [2];").unwrap();
+   tlc.check(Some(l1), "match 3 { 1 => 1, 2 => 2, _ => 3 } @reduce : [3];").unwrap();
+   tlc.check(Some(l1), "match 3 { 1 => 1, 2 => 2, _ => 3 } @reduce : [4];").unwrap_err();
+}
+
+#[test]
+fn check_iflet_literal() {
+   let mut tlc = TLC::new();
+   let l1 = tlc.import_file(None, "preludes/l1.tlc").unwrap();
+
+   tlc.check(Some(l1), "if let 1 = 1 { 1 } else { 2 } @reduce : [1];").unwrap();
+   tlc.check(Some(l1), "if let 1 = 2 { 1 } else { 2 } @reduce : [2];").unwrap();
+   tlc.check(Some(l1), "if let 1 = 1 { 1 } else { 2 } @reduce : [3];").unwrap_err();
+}
