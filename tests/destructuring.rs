7e61113a5194c53535099406bcd45326be35281a
Andrew Johnson <andrew@subarctic.org>
2022-12-10 16:01:17 -0700

    add test cases for literal destructuring

--- tests/destructuring.rs
+++ tests/destructuring.rs
@@ -20,3 +20,21 @@ fn check_iflet_literal() {
    tlc.check(Some(l1), "(if let 1 = 2 then 1 else 2) @reduce : [2];").unwrap();
    tlc.check(Some(l1), "(if let 1 = 1 then 1 else 2) @reduce : [3];").unwrap_err();
 }
+
+#[test]
+fn destructure_literal() {
+   let mut tlc = TLC::new();
+   tlc.check(None, r#"match 0 { literal '0' => literal '0' } : Integer @reduce : [0]"#).unwrap();
+   tlc.check(None, r#"match 0 { literal '0' => literal '0' } : Integer @reduce : [00]"#).unwrap_err();
+   tlc.check(None, r#"match 0 { literal '0'a => literal a } : Integer @reduce : [0]"#).unwrap();
+   tlc.check(None, r#"match 00 { literal "00" => literal "00" } : Integer @reduce : [0]"#).unwrap_err();
+   tlc.check(None, r#"match 00 { literal "00" => literal "00" } : Integer @reduce : [00]"#).unwrap();
+   tlc.check(None, r#"match 00 { literal "00"a => literal a } : Integer @reduce : [00]"#).unwrap();
+   tlc.check(None, r#"match 00 { literal '0' a => literal '0' a } : Integer @reduce : [00]"#).unwrap();
+   tlc.check(None, r#"match 00 { literal '0' a '0' => literal '0' a '0' } : Integer @reduce : [00]"#).unwrap();
+   tlc.check(None, r#"match 000 { literal '0' a '0' => literal '0' a '0' } : Integer @reduce : [000]"#).unwrap();
+   tlc.check(None, r#"match 0 { literal [0-9]a => literal a } : Integer @reduce : [0]"#).unwrap();
+   tlc.check(None, r#"match 0 { literal [0-9]a b => literal a b } : Integer @reduce : [0]"#).unwrap();
+   tlc.check(None, r#"match 00 { literal [0-9]a b [0-9]c => literal a b c } : Integer @reduce : [00]"#).unwrap();
+   tlc.check(None, r#"match 000 { literal [0-9]a b [0-9]c => literal a b c } : Integer @reduce : [000]"#).unwrap();
+}
