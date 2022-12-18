83bf2097c6d65ab00c94da5a36d6cede7566b667
Andrew Johnson <andrew@subarctic.org>
2022-12-18 16:12:00 -0700

    make a failing test case for htuple/tuple equivalence and projection

--- tests/l1.rs
+++ tests/l1.rs
@@ -1,13 +1,18 @@
 use lsts::tlc::TLC;
 
 #[test]
-fn value_has_self() {
+fn l1_homogenous_tuples() {
    let mut tlc = TLC::new();
    let alg = tlc.import_file(None, "preludes/l1.tlc").unwrap();
 
-   tlc.check(Some(alg), "[1] :Tensor<Integer>;").unwrap();
-   tlc.check(Some(alg), "[1,2] :Tensor<Integer>;").unwrap();
-   tlc.check(Some(alg), "[1,2,3] :Tensor<Integer>;").unwrap();
-   tlc.check(Some(alg), "[] :Tensor<Integer>;").unwrap();
-   tlc.check(Some(alg), "[1] :Tensor<Boolean>;").unwrap_err();
+   tlc.check(Some(alg), "(): Integer[0];").unwrap();
+   tlc.check(Some(alg), "(1): Integer[];").unwrap_err();
+   tlc.check(Some(alg), "(1): Integer[1];").unwrap();
+   tlc.check(Some(alg), "(1,2): Integer[1];").unwrap_err();
+   tlc.check(Some(alg), "(1,2): Integer[2];").unwrap();
+   tlc.check(Some(alg), "(1,2,3): Integer[2];").unwrap_err();
+   tlc.check(Some(alg), "(1,2,3): Integer[3];").unwrap();
+   tlc.check(Some(alg), "((1,2,3): Integer[3].0 @reduce :[1];").unwrap();
+   tlc.check(Some(alg), "((1,2,3): Integer[3].1 @reduce :[2];").unwrap();
+   tlc.check(Some(alg), "((1,2,3): Integer[3].2 @reduce :[3];").unwrap();
 }
