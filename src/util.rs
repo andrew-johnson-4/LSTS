4e75bb6d2ad7f99730ba603f282a07f8c51b8829
andrew-johnson-4 <andrew@subarctic.org>
2025-01-16 13:26:03 -0700

    LSTS is in LM

--- src/util.rs
+++
@@ -1,5 +0,0 @@
-
-
-pub fn type_of<T>(_: T) -> &'static str {
-   std::any::type_name::<T>()
-}
--- tests/debug_messaging.rs
+++
@@ -1,9 +0,0 @@
-use lsts::tlc::TLC;
-
-#[test]
-fn check_lookup_failure_messaging() {
-   let mut tlc = TLC::new();
-   let l1 = tlc.import_file(None, "preludes/l1.tlc").unwrap();
-
-   let _error = tlc.check(Some(l1), "x:Number").unwrap_err();
-}
